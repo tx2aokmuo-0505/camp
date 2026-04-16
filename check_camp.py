9a21c5bfd212a0d1c0c16d4c36f89d7d63fd5c20
tx2aokmuo-0505 <tx2aokmuo@gmail.com>
2026-04-16 20:13:37 +0900

    Update check_camp.py

--- check_camp.py
+++ check_camp.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-# あなたの検索URL（長野・山梨・岐阜）
+# 検索URL（長野・山梨・岐阜）
 URL = "https://www.nap-camp.com/list?locationList=%5B137%2C138%2C139%2C140%2C141%2C142%2C143%2C144%2C120%2C121%2C122%2C123%2C124%2C125%2C126%2C127%2C128%2C129%2C130%2C131%2C132%2C133%2C134%2C135%2C136%2C165%2C166%2C167%2C168%2C169%2C170%5D&checkIn=2026-08-13&checkOut=2026-08-15&sortId=21"
 
 # Discordの合言葉（Webhook）
@@ -10,7 +10,7 @@ DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 
 def send_discord(message):
     if DISCORD_WEBHOOK_URL:
-        # ここでDiscordに送信しているよ
+        # ここでDiscordに送信
         requests.post(DISCORD_WEBHOOK_URL, json={"content": message})
     else:
         print("WebhookのURLが設定されていないみたいだよ。GitHubのSecretsを確認して！")
